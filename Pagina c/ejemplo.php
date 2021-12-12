c8fc408bdc34b4e518b74c3dff8093559cb59f8f
Miguel-Villalpando <94716050+Miguel-Villalpando@users.noreply.github.com>
2021-12-12 15:47:58 -0600

    Add files via upload

---
+++ Pagina c/ejemplo.php
@@ -0,0 +1,28 @@
+                                REGISTRO DE USUARIOS
+
+Nombre : Roberto
+
+Año de nacimiento: 1996
+
+Sexo : masculino
+
+
+Nombre : Samanta 
+
+Año de nacimiento: 1998
+
+Sexo : femenino
+
+
+Nombre : Monica
+
+Año de nacimiento: 1997
+
+Sexo : femenino
+
+
+Nombre : Miguel
+
+Año de nacimiento: 1999
+
+Sexo : masculino
---
+++ Pagina c/formularios-required-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>date. Formularios. HTML. Páginas web HTML y hojas de estilo CSS. </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <p>Su nombre es <strong><code>"$#%-Name"</code></strong>.</p>
+  <p>Año nacimiento <strong><code>"$#%-Año"</code></strong>.</p>
+  <p>Es usted un <strong><code>"$#%-Sex"</code></strong>.</p>
+
+  <p><a href="formularios-required-1.html">Volver al formulario.</a></p>
+</body>
+</html>
